9c875022a56136b3cf87363bcbe7e90d20142134
Safayet Ullah <sojibmirzabd@gmail.com>
2025-08-05 09:02:04 +0600

    updated

--- events/apps.py
+++ events/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from django.core.management import call_command
-import os
+
 
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
-
-    def ready(self):
-        print("EventsConfig.ready() called") 
-        if os.environ.get('LOAD_FIXTURE_ON_STARTUP') == 'true':
-            try:
-                call_command('loaddata', 'data.json')
-                print("Data loaded successfully!")
-            except Exception as e:
-                print(f"Error loading data: {e}")
