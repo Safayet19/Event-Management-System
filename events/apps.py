49fe1b70d5143d243c7a5eaecfcd6467829c8771
Safayet Ullah <sojibmirzabd@gmail.com>
2025-08-05 08:44:24 +0600

    ....

--- events/apps.py
+++ events/apps.py
@@ -1,15 +1,16 @@
 from django.apps import AppConfig
 from django.core.management import call_command
 import os
-from django.db.utils import OperationalError
 
 class EventsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'events'
 
     def ready(self):
+        print("EventsConfig.ready() called") 
         if os.environ.get('LOAD_FIXTURE_ON_STARTUP') == 'true':
             try:
                 call_command('loaddata', 'data.json')
-            except OperationalError:
-                pass  # Database not ready yet
+                print("Data loaded successfully!")
+            except Exception as e:
+                print(f"Error loading data: {e}")
